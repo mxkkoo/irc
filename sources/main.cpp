5d26139b99737f4b9f02f69c034d42c2e44d0e0d
asxkaa <kelyany1403@gmail.com>
2026-09-07 15:00:08 +0200

    chore: main.cpp refactor, exit statuses

--- sources/main.cpp
+++ sources/main.cpp
@@ -15,19 +15,24 @@
 #include <signal.h>
 
 int	main(int ac, char* av[]) {
-	try {	
-		if (ac != 3) {
-			throw (std::invalid_argument("./ircserv [port] [password]"));
-		}
+//Checks arg format, sets the signal handler then starts the server
 
-		Server	serv(av);
+	if (ac != 3) {
+		std::cerr << "format: ./ircserv [port] [password]" << std::endl;
+		return (1);
+	}
 
-		signal(SIGINT, Server::handleSignal);
-		signal(SIGQUIT, Server::handleSignal);
+	Server	serv(av);
+
+	signal(SIGINT, Server::handleSignal);
+	signal(SIGQUIT, Server::handleSignal);
+
+	try {
 		serv.start();
 	}
 	catch(const std::exception& e) {
 		std::cerr << e.what() << std::endl;
+		return (2);
 	}
 
 	return (0);
